1e07952fc4c3de0ce95e297ea6f2ec5642d1ab69
Andrew Stern <andrewdstern16@gmail.com>
2022-03-14 15:46:29 -0600

    Enter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/personCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="Jane" lastName="Doe" age={ 48 } hairColor="Black" />
+      <PersonCard firstName="John" lastName="Smith" age={ 88 } hairColor="Brown" />
+      <PersonCard firstName="Micheal" lastName="Jordan" age={ 63 } hairColor="Black" />
+      <PersonCard firstName="Larry" lastName="Bird" age={ 68 } hairColor="Ash Blonde" />
     </div>
   );
 }
